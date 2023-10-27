c89c9d394a9259718233b1bbc22505f2a3cd7a85
ziad500 <ziad.gemy@gmail.com>
2023-10-27 14:34:55 +0200

    todo lists view

---
+++ lib/features/todo/presentation/components/views/all_todos_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/features/todo/presentation/components/todo_listview.dart';
+import 'package:todo_app/features/todo/presentation/controller/cubit/todo_cubit.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TodoCubit, TodoState>(
+      builder: (context, state) {
+        var cubit = TodoCubit.get(context);
+        if (state is GetAllTodosLoadingState) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (cubit.allTodos.isNotEmpty) {
+          return TodoListview(todos: cubit.allTodos);
+        } else {
+          return const Center(
+            child: Text("No data"),
+          );
+        }
+      },
+    );
+  }
+}
---
+++ lib/features/todo/presentation/components/views/done_todos_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/features/todo/presentation/components/todo_listview.dart';
+import 'package:todo_app/features/todo/presentation/controller/cubit/todo_cubit.dart';
+
+class DoneTodoList extends StatelessWidget {
+  const DoneTodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TodoCubit, TodoState>(
+      builder: (context, state) {
+        var cubit = TodoCubit.get(context);
+        if (state is GetAllTodosLoadingState) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (cubit.doneTodos.isNotEmpty) {
+          return TodoListview(todos: cubit.doneTodos);
+        } else {
+          return const Center(
+            child: Text("No data"),
+          );
+        }
+      },
+    );
+  }
+}
