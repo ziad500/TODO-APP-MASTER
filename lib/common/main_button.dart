fbbb98fb5053444a10fb5db08e1c38e5ab27727b
ziad500 <ziad.gemy@gmail.com>
2023-10-27 14:35:07 +0200

    main button

---
+++ lib/common/main_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/utils/app_colors.dart';
+
+class MainButton extends StatelessWidget {
+  const MainButton(
+      {super.key, required this.title, required this.icon, this.height = 60, this.onPressed});
+  final String title;
+  final IconData icon;
+  final double height;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.maxFinite,
+      height: height,
+      child: MaterialButton(
+        onPressed: onPressed,
+        color: AppColors.defaultColor,
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 25,
+            ),
+            Text(title),
+          ],
+        ),
+      ),
+    );
+  }
+}
