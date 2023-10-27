aa91e406ccb72fa39a3f4f2036c8f2d9d778efd0
ziad500 <ziad.gemy@gmail.com>
2023-10-27 14:36:49 +0200

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:todo_app/features/todo/data/database/todo_database.dart';
 import 'package:todo_app/features/todo/domain/entities/todo_model.dart';
 import 'package:todo_app/features/todo/presentation/controller/cubit/todo_cubit.dart';
 import 'package:todo_app/features/todo/presentation/screens/todos_screen.dart';
+import 'package:todo_app/utils/app_colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,16 +25,25 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
         providers: [
           BlocProvider(
-            create: (context) => di.sl<TodoCubit>(),
+            create: (context) => di.sl<TodoCubit>()..getAllTodos(),
           )
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: TodoScreen(),
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+              checkboxTheme:
+                  const CheckboxThemeData(fillColor: MaterialStatePropertyAll(Colors.black)),
+              tabBarTheme: TabBarTheme(
+                  dividerColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  labelColor: AppColors.defaultColor,
+                  unselectedLabelStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  labelStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  indicator: BoxDecoration(
+                      color: const Color(0xffD5DBFB), borderRadius: BorderRadius.circular(30)))),
+          home: const TodoScreen(),
         ));
   }
 }
