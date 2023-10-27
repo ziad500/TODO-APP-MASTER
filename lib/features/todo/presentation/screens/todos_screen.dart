7b7166b95a58fbd9dc92554d79d02c04c1a056a8
ziad500 <ziad.gemy@gmail.com>
2023-10-27 14:36:57 +0200

    todo screen

--- lib/features/todo/presentation/screens/todos_screen.dart
+++ lib/features/todo/presentation/screens/todos_screen.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/features/todo/domain/entities/todo_model.dart';
-import 'package:todo_app/features/todo/presentation/controller/cubit/todo_cubit.dart';
+import 'package:todo_app/common/main_button.dart';
+import 'package:todo_app/features/todo/presentation/components/add_todo_button.dart';
+import 'package:todo_app/features/todo/presentation/components/views/done_todos_view.dart';
+import 'package:todo_app/features/todo/presentation/components/todo_appbar.dart';
+import 'package:todo_app/features/todo/presentation/components/todo_search_bar.dart';
+import 'package:todo_app/features/todo/presentation/components/todo_tab_bar.dart';
+import 'package:todo_app/features/todo/presentation/components/views/all_todos_view.dart';
+import 'package:todo_app/features/todo/presentation/components/views/undone_todos_view.dart';
 
 class TodoScreen extends StatelessWidget {
   const TodoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-      ),
-      body: Column(
-        children: [
-          MaterialButton(
-            onPressed: () {
-              TodoCubit.get(context).addTodo(TodoModel(
-                  title: "title",
-                  description: "description",
-                  created: DateTime.now().toString(),
-                  status: "done".toString()));
-            },
-            color: Colors.amber,
-          )
-        ],
+    return const DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        floatingActionButton: AddTodoButton(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TodoAppbar(),
+                SizedBox(
+                  height: 16,
+                ),
+                TodoSearchBar(),
+                SizedBox(
+                  height: 16,
+                ),
+                TodoTabBar(),
+                SizedBox(
+                  height: 16,
+                ),
+                Expanded(
+                    child: TabBarView(children: [TodoList(), DoneTodoList(), UnDoneTodoList()]))
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
